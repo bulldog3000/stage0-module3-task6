6697f57e40f55cd4c9bd51b2960a5b6246a2231f
bulldog3000 <128056980+bulldog3000@users.noreply.github.com>
2023-10-10 00:07:16 +0300

    Add files via upload

---
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -0,0 +1,8 @@
+package lang.print.gaps.task6;
+
+public class FloatTypeCasting {
+    public void roundNumber(float numberToBeRounded) {
+        int res = Math.round(numberToBeRounded);
+        System.out.println(res);
+    }
+}
---
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -0,0 +1,10 @@
+package lang.print.gaps.task6;
+
+public class IntegerTypeCasting {
+    public static void main(String[] args) {
+        float first = 1000f;
+        int second = (int) first;
+        System.out.println(first);
+        System.out.println(second);
+    }
+}
